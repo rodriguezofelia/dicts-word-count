f25ec9126d92b19ba617d82575df2d03cfd1f676
Ofelia <oer.rodriguez@gmail.com>
2021-01-29 04:28:14 +0000

    Added words and their respective count to a dictionary

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,22 @@
 # put your code here.
+
+def word_count(filename):
+
+    poem = open(filename)
+
+    word_count = {}
+
+    for line in poem:
+        line = line.rstrip()
+        words = line.split(' ')
+
+
+        for word in words:
+            if word in word_count:
+                word_count[word] += 1
+            else: 
+                word_count[word] = 1
+
+    return word_count
+
+word_count("test.txt")
